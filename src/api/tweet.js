674cc726d2ffd4c35972b52c6a1272945644c0ce
franciscomarin98 <francmarinc@gmail.com>
2021-07-31 13:52:22 -0500

    Refactor de la accion de guardar

--- src/api/tweet.js
+++ src/api/tweet.js
@@ -2,11 +2,17 @@ import { v4 as uuidv4 } from 'uuid'
 import { LOCALSTORAGE_KEY } from '../utils/constants'
 
 export const saveTweetAPI = (tweet, userName) => {
+  const tweets = getTweetsAPI()
+
   const TweetTemp = {
     id: uuidv4(),
     tweet,
     userName,
     createdAt: new Date()
   }
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(TweetTemp))
+
+  tweets.push(TweetTemp)
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(tweets))
 }
+
+export const getTweetsAPI = () => JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || []
