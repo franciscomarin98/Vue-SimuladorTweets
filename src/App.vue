e04a0774ec937da61abb91a8429776a2fa359c87
franciscomarin98 <francmarinc@gmail.com>
2021-07-31 14:19:20 -0500

    Creando el template para renderizar la data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <MenuBar :openForm="openCloseForm" :showForm="showForm"/>
   <TweetForm :showForm="showForm" />
+  <TweetList />
 </template>
 
 <script>
 import MenuBar from './components/MenuBar'
 import TweetForm from './components/TweetForm'
 import useFormTweet from './composables/useFormTweet'
+import TweetList from './components/TweetList'
 
 export default {
   name: 'App',
   components: {
+    TweetList,
     TweetForm,
     MenuBar
   },
